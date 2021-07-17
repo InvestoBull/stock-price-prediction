64ad031dc2ce5a9338ef06dfca22c83cc06318b5
Raghav Thakur <raghav.thakur.rt.20@gmail.com>
2021-07-17 13:22:12 -0700

    Changed searcbar and icons to Chakra-UI

--- src/components/StockSearchBar/index.jsx
+++ src/components/StockSearchBar/index.jsx
@@ -1,4 +1,14 @@
-import { Box, Container, HStack, useColorModeValue } from '@chakra-ui/react';
+import {
+    Box,
+    Container,
+    HStack,
+    useColorModeValue,
+    Divider,
+    InputGroup,
+    InputRightElement,
+    InputLeftElement,
+    Input,
+} from '@chakra-ui/react';
 import { IoClose, IoSearch } from 'react-icons/io5';
 import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
@@ -26,58 +36,6 @@ const SearchInputContainer = styled.div`
     align-items: center;
     position: relative;
     padding: 2px 15px;
-`;
-
-const SearchInput = styled.input`
-    width: 100%;
-    height: 100%;
-    outline: none;
-    border: none;
-    font-size: 21px;
-    color: textColor;
-    font-weight: 500;
-    border-radius: 6px;
-    background-color: transparent;
-
-    &:focus {
-        outline: none;
-
-        &::placeholder {
-            opacity: 0;
-        }
-    }
-
-    &::placeholder {
-        color: #bebebe;
-        transition: all 250ms ease-in-out;
-    }
-`;
-
-const SearchIcon = styled.span`
-    color: #bebebe;
-    font-size: 27px;
-    margin-right: 10px;
-    margin-top: 6px;
-    vertical-align: middle;
-`;
-
-const CloseIcon = styled(motion.span)`
-    color: #bebebe;
-    font-size: 23px;
-    vertical-align: middle;
-    transition: all 200ms ease-in-out;
-    cursor: pointer;
-
-    &:hover {
-        color: #dfdfdf;
-    }
-`;
-
-const LineSeperator = styled.span`
-    display: flex;
-    min-width: 100%;
-    min-height: 2px;
-    background-color: #d8d8d878;
 `;
 
 const SearchContent = styled.div`
@@ -196,32 +154,39 @@ const StockSearchBar = (props) => {
                         ref={parentRef}
                     >
                         <SearchInputContainer>
-                            <SearchIcon>
-                                <IoSearch />
-                            </SearchIcon>
-                            <SearchInput
-                                placeholder='Search for stock'
-                                onFocus={expandContainer}
-                                ref={inputRef}
-                                value={searchQuery}
-                                onChange={changeHandler}
-                            />
-                            <AnimatePresence>
-                                {isExpanded && (
-                                    <CloseIcon
-                                        key='close-icon'
-                                        initial={{ opacity: 0 }}
-                                        animate={{ opacity: 1 }}
-                                        exit={{ opacity: 0 }}
-                                        transition={{ duration: 0.2 }}
-                                        onClick={collapseContainer}
-                                    >
-                                        <IoClose />
-                                    </CloseIcon>
-                                )}
-                            </AnimatePresence>
+                            <InputGroup>
+                                <InputLeftElement>
+                                    <IoSearch color='#bebebe' fontSize='27px' />
+                                </InputLeftElement>
+                                <Input
+                                    placeholder='Search for stock'
+                                    onFocus={expandContainer}
+                                    ref={inputRef}
+                                    value={searchQuery}
+                                    onChange={changeHandler}
+                                    boxShadow='none'
+                                    variant='filled'
+                                />
+                                <InputRightElement>
+                                    <AnimatePresence>
+                                        {isExpanded && (
+                                            <IoClose
+                                                key='close-icon'
+                                                color='#bebebe'
+                                                fontSize='23px'
+                                                cursor='pointer'
+                                                initial={{ opacity: 0 }}
+                                                animate={{ opacity: 1 }}
+                                                exit={{ opacity: 0 }}
+                                                transition={{ duration: 0.2 }}
+                                                onClick={collapseContainer}
+                                            />
+                                        )}
+                                    </AnimatePresence>
+                                </InputRightElement>
+                            </InputGroup>
                         </SearchInputContainer>
-                        {isExpanded && <LineSeperator />}
+                        {isExpanded && <Divider orientation='horizontal' />}
                         {isExpanded && (
                             <SearchContent>
                                 {isLoading && (
