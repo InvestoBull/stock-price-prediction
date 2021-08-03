bd4ffd9d9b3750f0c934c54bb1820d0e6e086184
Weiyoung Tan <wtan@bgcengineering.ca>
2021-08-03 01:06:35 -0700

    remove box around spinner

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,13 +1,6 @@
 import React from "react";
-import {Box, Center, Spinner, useColorModeValue} from "@chakra-ui/react";
+import { Spinner } from "@chakra-ui/react";
 
-const LoadingSpinner = () => (
-    <Center w='100%' minH={32}>
-        <Box borderRadius="xl" border='2px' padding={2}
-             borderColor={useColorModeValue('brand.400', 'brand.600')}>
-            <Spinner/>
-        </Box>
-    </Center>
-)
+const LoadingSpinner = () => <Spinner size="xl" />;
 
 export default LoadingSpinner;
